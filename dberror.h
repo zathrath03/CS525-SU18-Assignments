981703c00a3e22b460abdca126efc901df173e12
Amer Alsabbagh <aalsabbagh@hawk.iit.edu>
2018-07-06 21:51:52 -0500

    --- added source files from Blackboard

--- dberror.h
+++ dberror.h
@@ -14,20 +14,6 @@ typedef int RC;
 #define RC_FILE_HANDLE_NOT_INIT 2
 #define RC_WRITE_FAILED 3
 #define RC_READ_NON_EXISTING_PAGE 4
-#define RC_STORAGE_MANAGER_NOT_INITIALIZED 5
-#define RC_FILE_CREATION_FAILED 6
-#define RC_WRITE_TO_OUTPUTSTREAM_FAILED 7
-#define RC_INCOMPATIBLE_BLOCKSIZE 8
-#define RC_FILE_NOT_INITIALIZED 9
-#define RC_FILE_OFFSET_FAILED 10
-#define RC_FILE_READ_FAILED 11
-#define RC_FILE_NOT_CLOSED 12
-#define RC_CURRENT_PAGE_NOT_FOUND 13
-#define RC_NO_FILENAME 14
-#define RC_MISMATCH 15
-#define RC_PINNED_NOT_OUT 16
-#define RC_MATCH 17
-
 
 #define RC_RM_COMPARE_VALUE_OF_DIFFERENT_DATATYPE 200
 #define RC_RM_EXPR_RESULT_IS_NOT_BOOLEAN 201
@@ -49,23 +35,23 @@ extern void printError (RC error);
 extern char *errorMessage (RC error);
 
 #define THROW(rc,message) \
-  do {			  \
-    RC_message=message;	  \
-    return rc;		  \
-  } while (0)		  \
+		do {			  \
+			RC_message=message;	  \
+			return rc;		  \
+		} while (0)		  \
 
 // check the return code and exit if it is an error
 #define CHECK(code)							\
-  do {									\
-    int rc_internal = (code);						\
-    if (rc_internal != RC_OK)						\
-      {									\
-	char *message = errorMessage(rc_internal);			\
-	printf("[%s-L%i-%s] ERROR: Operation returned error: %s\n",__FILE__, __LINE__, __TIME__, message); \
-	free(message);							\
-	exit(1);							\
-      }									\
-  } while(0);
+		do {									\
+			int rc_internal = (code);						\
+			if (rc_internal != RC_OK)						\
+			{									\
+				char *message = errorMessage(rc_internal);			\
+				printf("[%s-L%i-%s] ERROR: Operation returned error: %s\n",__FILE__, __LINE__, __TIME__, message); \
+				free(message);							\
+				exit(1);							\
+			}									\
+		} while(0);
 
 
 #endif
