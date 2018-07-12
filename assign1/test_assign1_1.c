1181ddc5d400e56085bb3e4076f707f2f1d3264a
DESKTOP-7D753A7\Syed <sjagird1@hawk.iit.edu>
2018-07-11 19:01:22 -0500

    READ and WRITE tests completed; As well as ensureCapacity test completed.  Only need to complete appendEmptyBlock test and getBlockPos test which will be done either tonight or tomorrow morning.

--- assign1/test_assign1_1.c
+++ assign1/test_assign1_1.c
@@ -22,6 +22,7 @@ static void testPageInfo(void);
 static void testEnsureCapacity(void);
 static void testAppendEmptyBlock(void);
 static void testErrorCases(void);
+static void testReadWrite(void);
 
 /* main function running all tests */
 int
@@ -36,6 +37,7 @@ main (void) {
 //    testEnsureCapacity();
 //    testAppendEmptyBlock();
     testErrorCases();
+    testReadWrite();
 
     return 0;
 }
@@ -130,6 +132,150 @@ testSinglePageContent(void) {
 
     TEST_DONE();
 }
+
+/*****************************************************************************
+          testReadWrite will test the certain functions of read and write
+
+          Specifically: READ functions:
+                        ---------------
+                        ~readBlock----------[done]
+                        ~readCurrentBlock---[done]
+                        ~readFirstBlock-----[done]
+                        ~readLastBlock------[done]
+                        ~readNextBlock------[done]
+                        ~readPreviousBlock--[done]
+
+                        WRITE functions:
+                        ----------------
+                        ~writeBlock---------[done]
+                        ~writeCurrentBlock--[done]
+
+          Additionally: ~ensureCapacity-----[done]
+******************************************************************************/
+void
+testReadWrite(void) {
+    SM_FileHandle fh;
+    SM_PageHandle ph;
+    int i=0, counter = 0;
+    testName = "test READ and WRITE functionality";
+    printf("\n\t\tTESTING READ AND WRITE FUNCTIONALITY\n");
+    ph = (SM_PageHandle) malloc(PAGE_SIZE);
+
+    // create a new page file
+    TEST_CHECK(createPageFile (TESTPF));
+    TEST_CHECK(openPageFile (TESTPF, &fh));
+    printf("Create and Open new page file");
+    //Testing readFirstBlock successful
+    TEST_CHECK(readFirstBlock (&fh, ph));
+    // the page should be empty (zero bytes)
+    for (i=0; i < PAGE_SIZE; i++){
+        //using an if with an error counter to prevent spamming the output with the ASSERT_TRUE messages
+        if(ph[i] != 0)
+            counter ++;
+    }
+    //Check if first block is empty
+    ASSERT_TRUE((counter == 0), "expected zero in every byte of the freshly initialized page");
+    printf("first block was empty\n");
+    //Check if correct number of pages exist
+    ASSERT_TRUE((fh.totalNumPages == 1), "expect 1 page in new file");
+    printf("Extend the number of pages to 5");
+    //Make sure we have enough pages to work with
+    TEST_CHECK(ensureCapacity(5, &fh));
+    ASSERT_TRUE((fh.totalNumPages == 5), "Correct amount of pages exist");
+
+    //Writing blocks into file which will be used for reading
+    for (i=0; i < PAGE_SIZE; i++)
+        ph[i] = '0';
+    TEST_CHECK(writeBlock(0, &fh, ph));
+    printf("writing into block %d \n", fh.curPagePos);
+    //Writing into the next page
+    for (i=0; i < PAGE_SIZE; i++)
+        ph[i] = '1';
+    writeBlock (1, &fh, ph);
+    printf("writing into block %d \n", fh.curPagePos);
+    //Writing in the third page
+    for (i=0; i < PAGE_SIZE; i++)
+        ph[i] = '2';
+    writeBlock (2, &fh, ph);
+    printf("writing into block %d \n", fh.curPagePos);
+    //Writing in the fourth page
+    for (i=0; i < PAGE_SIZE; i++)
+        ph[i] = '3';
+    writeBlock (3, &fh, ph);
+    printf("writing into block %d \n", fh.curPagePos);
+        //Writing in the third page
+    for (i=0; i < PAGE_SIZE; i++)
+        ph[i] = '4';
+    writeBlock (4, &fh, ph);
+    printf("writing into block %d \n", fh.curPagePos);
+    printf("\n");
+    //Test read functions
+
+    //Testing readBlock
+    printf("Testing 'readBlock' by reading third block\n");
+    TEST_CHECK(readBlock(2,&fh, ph));
+    for (i=0; i < PAGE_SIZE; i++){
+        if(ph[i] !='2')
+            counter++;
+    }
+    ASSERT_TRUE((counter == 0), "Read the third block successfully\n");
+
+    //Testing readFirstBlock
+    printf("Testing 'readFirstBlock' by reading the first block\n");
+    TEST_CHECK(readFirstBlock(&fh, ph));
+    for (i=0; i < PAGE_SIZE; i++){
+        if(ph[i] !='0')
+            counter++;
+    }
+    ASSERT_TRUE((counter == 0), "Read the first block successfully\n");
+
+    //Testing readLastBlock
+    printf("Testing 'readLastBlock' by reading the last block\n");
+    TEST_CHECK(readLastBlock(&fh, ph));
+    for (i=0; i < PAGE_SIZE; i++){
+        if(ph[i] !='4')
+            counter++;
+    }
+    ASSERT_TRUE((counter == 0), "Read the last block successfully\n");
+
+    //Testing readCurrentBlock
+    fh.curPagePos = 0;
+    printf("Testing 'readCurrentBlock' by reading the current block which is set to be block 0\n");
+    TEST_CHECK(readCurrentBlock(&fh, ph));
+    for (i=0; i < PAGE_SIZE; i++){
+        if(ph[i] !='0')
+            counter++;
+    }
+    ASSERT_TRUE((counter == 0), "Read the current block (first block) successfully\n");
+
+    //Testing readPreviousBlock
+    fh.curPagePos = 3;
+    printf("Testing 'readPreviousBlock' by reading the previous block which is the 2nd block\n");
+    TEST_CHECK(readPreviousBlock(&fh, ph));
+    for (i=0; i < PAGE_SIZE; i++){
+        if(ph[i] !='2')
+            counter++;
+    }
+    ASSERT_TRUE((counter == 0), "Read the previous block (block 2) successfully\n");
+
+    //Testing readNextBlock
+    printf("Testing 'readNextBlock' by reading the next block which is the 4th block\n");
+    TEST_CHECK(readNextBlock(&fh, ph));
+    for (i=0; i < PAGE_SIZE; i++){
+        if(ph[i] !='4')
+            counter++;
+    }
+    ASSERT_TRUE((counter == 0), "Read the next block (block 4) successfully\n");
+
+
+    //close page file before delete
+    TEST_CHECK(closePageFile (&fh));
+    // destroy new page file
+    TEST_CHECK(destroyPageFile (TESTPF));
+    TEST_DONE();
+}
+
+
 /*****************************************************************************
           testPageInfo will test the certain steps taken in writeBlock
 ******************************************************************************/
@@ -143,10 +289,12 @@ testPageInfo(void) {
     ph = (SM_PageHandle) malloc(PAGE_SIZE);
 
     TEST_CHECK(createPageFile (TESTPF));
+    printf("File created\n");
     TEST_CHECK(openPageFile (TESTPF, &fh));
+    printf("File opened\n");
     TEST_CHECK(writeBlock(0, &fh, ph));
 
-    //test mgmtInfo
+    //Check if file handle's file is initialized
     ASSERT_TRUE((fh.mgmtInfo), "File handle's file is initialized");
 
     //test block size
@@ -154,7 +302,9 @@ testPageInfo(void) {
     if(stat(fh.fileName,&st)!= 0){
       return RC_FILE_NOT_INITIALIZED;
     }
+    //Check if, after filling a page with null elements, will it equal 4096 bytes
     ASSERT_TRUE((PAGE_SIZE == st.st_size), "Page size is correct");
+
 
     TEST_CHECK(closePageFile (&fh));
     // destroy new page file
