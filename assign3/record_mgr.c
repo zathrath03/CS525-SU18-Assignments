d6b6e3908462ddda8dd783015a04b0603bd8c986
JDuggerIIT <jdugger@iit.edu>
2018-08-01 14:15:06 -0500

    *Changed all PageFileHeader variables to unsigned *Added a RM_Schema struct... not sure if I'm ever actually going to use it *Debugged ASSERT_RC_OK macro *Initial implementation of createTable() *Defined offset macros for reading data from the page header

--- assign3/record_mgr.c
+++ assign3/record_mgr.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
+#include <io.h>
 
 #include "bitmap.h"
 #include "record_mgr.h"
 #include "buffer_mgr.h"
+#include "storage_mgr.h"
 
-
-
-
-// Macros
+/*********************************************************************
+*
+*                             MACROS
+*
+*********************************************************************/
 #define VALID_CALLOC(type, varName, number, size)   \
     type *varName = (type *) calloc(number, size);  \
     if(!varName){                                   \
@@ -16,15 +20,33 @@
         exit(-1);                                   \
     }
 
+/**Must declare RC returnCode in function before using ASSERT_RC_OK**/
 #define ASSERT_RC_OK(functionCall)          \
-    RC returnCode = functionCall;                 \
+    returnCode = functionCall;                 \
     if(returnCode != RC_OK )\
        return returnCode;
 
+/*********************************************************************
+Offset Macros for retrieving data from the PageFile header
+*********************************************************************/
+#define recordSizeOffset 0
+#define numTuplesOffset sizeof(unsigned short)
+#define nextFreePageOffset numTuplesOffset + sizeof(unsigned int)
+#define numSlotsPerPageOffset nextFreePageOffset + sizeof(unsigned int)
+#define schemaSizeOffset numSlotsPerPageOffset + sizeof(unsigned short)
+#define schemaOffset schemaSizeOffset + sizeof(unsigned short)
+#define numAttrOffset schemaOffset
+#define dataTypeOffset(i) schemaOffset + ((2*i)+1)*sizeof(unsigned short)
+#define typeLengthOffset(i) schemaOffset + 2*(i+1)*sizeof(unsigned short)
+/**keySizeOffset requires defining numAttr**/
+#define keySizeOffset schemaOffset + ((2*numAttr)+1)*sizeof(unsigned short)
+#define keyAttrOffset(i) keySizeOffset + i*sizeof(unsigned short)
+/**firstNameOffset requires defining keySize**/
+#define firstNameOffset keySizeOffset + keySize*sizeof(unsigned short)
 
 // Prototypes for helper functions
 int static findFreeSlot(bitmap * bitMap);
-
+static RC preparePFHdr(Schema *schema, SM_PageHandle *pHandle);
 
 /*********************************************************************
 *
@@ -57,14 +79,27 @@ INPUT:
     schema: fully initialized schema
 *********************************************************************/
 RC createTable (char *name, Schema *schema){
+    RC returnCode = RC_INIT;
     //validate input
+    if(!name || !schema)
+        return RC_RM_INIT_ERROR;
     //make sure a page file with that name doesn't already exist
+    if(!access(name, F_OK))
+        return RC_RM_FILE_ALREADY_EXISTS;
     //create a page file
+    ASSERT_RC_OK(createPageFile(name));
     //open the page file
-    //**write page file header**
-        //rm_serializer.c has a serializeSchema() function that
-        //converts the schema to a string
+    SM_FileHandle fHandle;
+    ASSERT_RC_OK(openPageFile(name, &fHandle));
+    //write page file header
+    VALID_CALLOC(SM_PageHandle, pHandle, 1, PAGE_SIZE);
+    ASSERT_RC_OK(preparePFHdr(schema, pHandle));
+    ASSERT_RC_OK(writeBlock(0, &fHandle, *pHandle));
     //close the page file
+    ASSERT_RC_OK(closePageFile(&fHandle));
+    //free allocated memory
+    free(pHandle);
+    pHandle = NULL;
     return RC_OK;
 }
 
@@ -139,6 +174,7 @@ INPUT:
     *record: id is null, *data contains record to insert
 *********************************************************************/
 RC insertRecord (RM_TableData *rel, Record *record){
+    RC returnCode = RC_INIT;
     //validate input
     if(!rel)
         return RC_RM_INIT_ERROR;
@@ -336,4 +372,121 @@ int static findFreeSlot(bitmap * bitMap){
         }
     }
     return mapSize;
+}
+
+/*********************************************************************
+preparePFHdr populates a PageHandle with the data for the PageFile Hdr
+Assumes initial generation of pageFile, so no tuples have been added
+INPUT:
+    *schema: fully initialized Schema struct
+    *pHandle: uninitialized PageHandle with PAGE_SIZE memory alloc'd
+FORMAT:
+DataType and TypeLength pairs are repeated numAttr times
+keyAttr is repeated keySize times
+strlen is the length of the subsequent attribute name
+strlen and attrName pairs are repeated numAttr times
+
+---------------------------------------------------------------------------
+ushort recordSize | uint numTuples | uint nextFreePage |
+---------------------------------------------------------------------------
+ushort numSlotsPerPage | ushort schemaSize | ushort numAttr |
+---------------------------------------------------------------------------
+ushort DataType | ushort TypeLength | ushort DataType | ushort TypeLength |
+---------------------------------------------------------------------------
+... | ushort keySize | ushort keyAttr | ushort keyAttr | ... |
+---------------------------------------------------------------------------
+ushort strlen | char* attrName | ushort strlen | char* attrName | ... |
+---------------------------------------------------------------------------
+OFFSETS:
+    recordSize: 0
+    numTuples: sizeof(ushort)
+    nextFreePage: sizeof(ushort) + sizeof(uint)
+    numSlotsPerPage: sizeof(ushort) + 2*sizeof(uint)
+    schemaSize: 2*( sizeof(ushort) + sizeof(uint) )
+    schema: 3*sizeof(ushort) + 2*sizeof(uint)
+
+SCHEMA OFFSETS FROM START OF SCHEMA
+    numAttr: 0
+    DataType of attribute i: 2*i*sizeof(ushort) + sizeof(ushort)
+    TypeLength of attribute i: 2*(i+1)*sizeof(ushort)
+    keySize: (2*numAttr + 1) * sizeof(ushort)
+    keyAttr i: (2*numAttr + 2 + i) * sizeof(ushort)
+    names: (2*numAttr + 2 + keySize * i) * sizeof(ushort)
+    Offset to a specific attribute's name will need to be calculated
+        using the strlen's
+*********************************************************************/
+static RC preparePFHdr(Schema *schema, SM_PageHandle *pHandle){
+    //validate input
+    if(!schema || !pHandle)
+        return RC_RM_INIT_ERROR;
+
+    //Generate Info for Header
+    unsigned short recordSize = (unsigned short) getRecordSize(schema);
+    unsigned int numTuples = 0;
+    unsigned int nextFreePage = 1;
+    unsigned short numSlotsPerPage = (unsigned short) (PAGE_SIZE / recordSize);
+
+    //Retrieve existing data from schema
+    unsigned short numAttr = (unsigned short) schema->numAttr;
+    unsigned short keySize = (unsigned short) schema->keySize;
+
+    //Allocate memory for the schema arrays
+    VALID_CALLOC(unsigned short, typeAndLength, 2*numAttr, sizeof(unsigned short));
+    VALID_CALLOC(unsigned short, keyAttrs, keySize, sizeof(unsigned short));
+
+    //Initialize variables to use for retrieving string data from schema
+    unsigned short sizeOfAttrNames = 0;
+    VALID_CALLOC(unsigned short, strLen, numAttr, sizeof(unsigned short));
+
+    //Retrieve data from arrays and format how we want
+    for(int i = 0; i < numAttr; i++){
+        typeAndLength[2*i] = (unsigned short) schema->dataTypes[i];
+        typeAndLength[(2*i)+1] = (unsigned short) schema->typeLength[i];
+        strLen[i] = (unsigned short) strlen(schema->attrNames[i]);
+        sizeOfAttrNames += strLen[i];
+    }
+
+    //Calculate the schema size from its components
+    unsigned short schemaSize = (2 + 3*numAttr + keySize) * sizeof(unsigned short) + sizeOfAttrNames;
+
+    //Populating the pageHandle with the data
+    unsigned short curOffset = 0;
+
+    *pHandle[curOffset] = recordSize;
+    curOffset += sizeof(recordSize);
+    *pHandle[curOffset] = numTuples;
+    curOffset += sizeof(numTuples);
+    *pHandle[curOffset] = nextFreePage;
+    curOffset += sizeof(nextFreePage);
+    *pHandle[curOffset] = numSlotsPerPage;
+    curOffset += sizeof(numSlotsPerPage);
+    *pHandle[curOffset] = schemaSize;
+    curOffset += sizeof(schemaSize);
+    *pHandle[curOffset] = numAttr;
+    curOffset += sizeof(numAttr);
+
+    for(int i = 0; i < 2*numAttr; i++){
+        *pHandle[curOffset] = typeAndLength[i];
+        curOffset += sizeof(typeAndLength[i]);
+    }
+
+    *pHandle[curOffset] = keySize;
+    curOffset += sizeof(keySize);
+
+    for(int i = 0; i<keySize; i++){
+        *pHandle[curOffset] = (unsigned short) schema->keyAttrs[i];
+        curOffset += sizeof((unsigned short) schema->keyAttrs[i]);
+    }
+
+    for(int i = 0; i < numAttr; i++){
+        *pHandle[curOffset] = strLen[i];
+        curOffset += sizeof(strLen[i]);
+        pHandle[curOffset] = schema->attrNames[i];
+        curOffset += strLen[i];
+    }
+
+    free(typeAndLength);
+    free(keyAttrs);
+    free(strLen);
+    return RC_OK;
 }
